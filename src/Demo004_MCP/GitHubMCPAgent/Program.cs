4e6c8ae6083ef7766e029eddb5bc1058fd699f2a
Marco Antonio Silva <marconsilva@outlook.com>
2025-05-27 20:35:35 +0100

    removed diagnosis from mcp gh demo

--- src/Demo004_MCP/GitHubMCPAgent/Program.cs
+++ src/Demo004_MCP/GitHubMCPAgent/Program.cs
@@ -55,42 +55,42 @@ foreach (var tool in tools)
 //Full list of Supported Connectors: https://learn.microsoft.com/en-us/semantic-kernel/get-started/supported-languages?pivots=programming-language-csharp
 var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
 
-var resourceBuilder = ResourceBuilder
-    .CreateDefault()
-    .AddService("TelemetryConsoleQuickstart");
+// var resourceBuilder = ResourceBuilder
+//     .CreateDefault()
+//     .AddService("TelemetryConsoleQuickstart");
 
-// Enable model diagnostics with sensitive data.
-AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+// // Enable model diagnostics with sensitive data.
+// AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
 
-using var traceProvider = Sdk.CreateTracerProviderBuilder()
-    .SetResourceBuilder(resourceBuilder)
-    .AddSource("Microsoft.SemanticKernel*")
-    .AddConsoleExporter()
-    .Build();
+// using var traceProvider = Sdk.CreateTracerProviderBuilder()
+//     .SetResourceBuilder(resourceBuilder)
+//     .AddSource("Microsoft.SemanticKernel*")
+//     .AddConsoleExporter()
+//     .Build();
 
-using var meterProvider = Sdk.CreateMeterProviderBuilder()
-    .SetResourceBuilder(resourceBuilder)
-    .AddMeter("Microsoft.SemanticKernel*")
-    .AddConsoleExporter()
-    .Build();
+// using var meterProvider = Sdk.CreateMeterProviderBuilder()
+//     .SetResourceBuilder(resourceBuilder)
+//     .AddMeter("Microsoft.SemanticKernel*")
+//     .AddConsoleExporter()
+//     .Build();
 
-using var loggerFactory = LoggerFactory.Create(builder =>
-{
-    // Add OpenTelemetry as a logging provider
-    builder.AddOpenTelemetry(options =>
-    {
-        options.SetResourceBuilder(resourceBuilder);
-        options.AddConsoleExporter();
-        // Format log messages. This is default to false.
-        options.IncludeFormattedMessage = true;
-        options.IncludeScopes = true;
-    });
-    builder.SetMinimumLevel(LogLevel.Information);
-});
+// using var loggerFactory = LoggerFactory.Create(builder =>
+// {
+//     // Add OpenTelemetry as a logging provider
+//     builder.AddOpenTelemetry(options =>
+//     {
+//         options.SetResourceBuilder(resourceBuilder);
+//         options.AddConsoleExporter();
+//         // Format log messages. This is default to false.
+//         options.IncludeFormattedMessage = true;
+//         options.IncludeScopes = true;
+//     });
+//     builder.SetMinimumLevel(LogLevel.Warning);
+// });
 
-builder.Services.AddSingleton(loggerFactory);
+//builder.Services.AddSingleton(loggerFactory);
 
-builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Debug));
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Warning));
 
 Kernel kernel = builder.Build();
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
